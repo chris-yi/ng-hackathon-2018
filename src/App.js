601d658047f147190de3bc9dc88c7e07641e3c09
Huy Dang <>
2018-04-17 20:10:07 -0600

    merge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+   sendText = async (e) => {
+    console.log(" here is the message", this.refs.text.value ) 
+    this.refs.text.value = "";
+  }
+  onEnter = (e) => {
+    if(e.which === 13){
+      this.sendText()
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="text-display-container">
         </div>
         <div className="text-input-container">
-          <form>
-            <input type="text" className="text-input" />
-            <button>Submit</button>
-          </form>
+            <input type="text" className="text-input" ref="text" onKeyPress={this.onEnter}/>
+            <button onClick={this.sendText}>Submit</button>
         </div>
       </div>
     );
