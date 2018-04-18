3dd5e34ea81af642ce67160052ba2428de92fafb
Huy Dang <>
2018-04-18 08:51:25 -0600

    finish but have to fix setTimeout

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ class App extends Component {
 	state = {
 		mood: 2,
 		emojis: ['https://cdn.shopify.com/s/files/1/1061/1924/products/Cold_Sweat_Emoji_large.png?v=1480481051', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Anguished_Face_Emoji_large.png?v=1480481051', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Face_without_Mouth_large.png?v=1480481052', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Kiss_Emoji_with_Closed_Eyes_large.png?v=1480481054', 'https://cdn.shopify.com/s/files/1/1061/1924/products/Heart_Eyes_Emoji_large.png?v=1480481053'],
-		textMessages: [{ name: 'Kris', text: 'Hey Babe <3' }, {
-			name: 'Jessica',
-			text: ' Ugh, I\'m really dissapointed in you a*******!',
-		}],
-	};
+		textMessages: [],
+  };
+  
 	sendText = () => {
 		const itemsRef = firebase.database().ref('items');
 		const item = {
 			text: this.refs.text.value,
-			name: 'danny',
+			name: 'Chris',
 		};
 		itemsRef.push(item);
 		this.refs.text.value = ''
@@ -25,7 +23,22 @@ class App extends Component {
 	onEnter = (e) => {
 		if (e.which === 13) {
 			this.sendText();
-		}
+    }
+      setTimeout(() => {
+        this.waitForMoodCheck()
+      }, 2000)
+      // var timer = null;
+
+      // if (timer != null) {
+      //   window.clearTimeout(timer); 
+      //   timer = null;
+      // }
+      // else {
+      //   timer = window.setTimeout(() => {
+      //     this.waitForMoodCheck()
+      //   }, 2000);
+      // }
+      
 	};
 
 	componentDidMount() {
@@ -46,6 +59,54 @@ class App extends Component {
 		});
 	}
 
+
+  checkTheMood = async (message) => {
+    const payload = {
+      documents: [
+        {
+          language: "en",
+          id: "0",
+          text: message
+        }
+      ]
+    }
+    return axios.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment", payload, {headers: {"Ocp-Apim-Subscription-Key": "a51c5282a8e949a7b783e38ce7b020b0"}})
+  }
+
+  waitForMoodCheck = async () => {
+    const moodData = await this.checkTheMood(this.refs.text.value)
+    console.log("hello", moodData.data)
+    if(this.refs.text.value.length > 1){
+      let theRealMood = moodData.data.documents[0].score
+      if(theRealMood > 0.8){
+        this.setState({
+          mood: 4
+        })
+      } else if (theRealMood > 0.6){
+        this.setState({
+          mood: 3
+        })
+      } else if (theRealMood > 0.4){
+        this.setState({
+          mood: 2
+        })
+      } else if (theRealMood > 0.2){
+        this.setState({
+          mood: 1
+        })
+      } else {
+        this.setState({
+          mood: 0
+        })
+      }
+    } else {
+      this.setState({
+        mood: 2
+      })
+    }
+  }
+
+ 
 	render() {
 		console.log('textMessages', this.state.textMessages);
 		const message = this.state.textMessages.map((message) => {
@@ -70,7 +131,6 @@ class App extends Component {
 				</div>
 			</div>
 		);
-	}
-}
+}}
 
 export default App;
