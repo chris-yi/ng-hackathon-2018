556972096024a86474cec1208905f2ab4c171263
Chris Yi <chris@jjumpp.com>
2018-04-17 19:36:55 -0600

    basic front

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Sentimentaly</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="text-display-container">
+        </div>
+        <div className="text-input-container">
+          <form>
+            <input type="text" className="text-input" />
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
